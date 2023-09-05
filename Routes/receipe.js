68f22849dd097444edc1343f53a7d2e5f9812677
Shruthajshetty <109536307+Shruthajshetty@users.noreply.github.com>
2023-09-05 16:33:51 +0530

    Delete Routes directory

--- Routes/receipe.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const recipeController = require("../Controllers/recipe.controller");
-
-router.post("/addreciepe", recipeController.createRecipe);
-router.get("/", recipeController.getAllRecipe);
-
-module.exports = router;
